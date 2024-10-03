db795bbda2fdae4c0eb358234082657291d36833
Fabiola <suarezfabiola17@gmail.com>
2024-10-03 20:23:10 -0300

    Estilos al home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { arrowRedoOutline } from 'ionicons/icons';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonIcon,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ arrowRedoOutline });
+  }
 }
