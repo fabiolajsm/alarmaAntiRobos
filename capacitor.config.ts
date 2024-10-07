44751b4e89f8722fec29fd5724dfed3b6b201b28
Fabiola <suarezfabiola17@gmail.com>
2024-10-06 21:13:32 -0300

    Cambio de id de la app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.starter.alarmaAntiRobos.pps',
   appName: 'Alarma Anti Robos',
-  webDir: 'www'
+  webDir: 'www',
 };
 
 export default config;
